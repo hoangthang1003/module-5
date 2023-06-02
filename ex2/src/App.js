70b6ef3cb7bf1d971e1487c4787d01ff788a8153
hoangthang1003 <110215092+hoangthang1003@users.noreply.github.com>
2023-06-02 13:56:41 +0700

    nopbai

---
+++ ex2/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import {Exercise} from "./component/Ex1";
+
+function App() {
+  return (
+   <>
+   <Exercise />
+   </>
+  );
+}
+
+export default App;
---
+++ ex2/src/component/Ex1.js
@@ -0,0 +1,60 @@
+export function Exercise() {
+    const posts = [
+        {
+            "title": "5 Best Crypto Performers During The 2022 Market Flop",
+            "slug": "5-best-crypto-performers-during-the-2022-market-flop",
+            "category": "Crypto News",
+            "updatedAt": "21 hours ago"
+        },
+        {
+            "title": " Top crypto funding stories of 2022  ",
+            "slug": "top-crypto-funding-stories-of-2022",
+            "category": "New Year Special",
+            "updatedAt": "a day ago"
+        },
+        {
+            "title": " 2023 will see the death of play-to-earn gaming ",
+            "slug": "2023-will-see-the-death-of-playtoearn-gaming",
+            "category": " Opinion",
+            "updatedAt": "a day ago"
+        },
+        {
+            "title": " US lawmakers under pressure following FTX collapse: Report  ",
+            "slug": "us-lawmakers-under-pressure-following-ftx-collapse-report",
+            "category": " News",
+            "updatedAt": "2 days ago"
+        },
+        {
+            "title": "A Crypto Holiday Special: Past, Present, And Future With Material Indicators",
+            "slug": "a-crypto-holiday-special-past-present-and-future-with-material-indicators",
+            "category": "Interviews",
+            "updatedAt": "2 days ago"
+        }
+    ]
+    return (
+        <>
+            <table className="table table-striped">
+                <tr>
+                    <th>Id</th>
+                    <th>Title</th>
+                    <th>Slug</th>
+                    <th>Category</th>
+                    <th>Update Date</th>
+                </tr>
+
+                {
+                    posts.map((value, index) =>
+                        <tr key={index}>
+                            <td>{index}</td>
+                            <td>{value.title}</td>
+                            <td>{value.slug}</td>
+                            <td>{value.category}</td>
+                            <td>{value.updatedAt}</td>
+                        </tr>
+                    )
+                }
+
+            </table>
+        </>
+    )
+}
