739099c2b579a1946383f23486325a25fcf55ed2
hoangthang1003 <110215092+hoangthang1003@users.noreply.github.com>
2023-06-06 20:32:35 +0700

    nopbai

---
+++ case-study/src/component/customer/Customer.js
@@ -0,0 +1,46 @@
+import {useEffect, useState} from "react";
+import {findAll} from "../../service/CustomerService";
+
+export function CustomerList() {
+    const [customers,setCustomers] = useState([])
+    const [idDelete,setIdDelete]= useState(null)
+    const [nameDelete,setNameDelete]= useState(null)
+    const customerList = async () => {
+        const result = await findAll()
+        setCustomers(result)
+    }
+    useEffect(()=>{
+        customerList()
+    },[])
+    return(
+        <>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Gender</th>
+                <th>Birth Day</th>
+                <th>Id Card</th>
+                <th>Phone</th>
+                <th>Email</th>
+                <th>Address</th>
+                <th>Customer Type</th>
+            </tr>
+            {
+                customers.map((customer,index)=>(
+                    <tr key={index}>
+                        <td>{customer.name}</td>
+                        <td>{customer.gender}</td>
+                        <td>{customer.dataOfBirth}</td>
+                        <td>{customer.idCard}</td>
+                        <td>{customer.phone}</td>
+                        <td>{customer.email}</td>
+                        <td>{customer.address}</td>
+                        <td>{customer.customerType?.name}</td>
+                    </tr>
+                ))
+            }
+        </table>
+        </>
+    )
+}
---
+++ case-study/src/service/CustomerService.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export const findAll = async () => {
+    const result = await axios.get(`http://localhost:2000/customer`)
+    return result.data
+}
+export const deleteById = async (id) => {
+    const result = await axios.delete(`http://localhost:2000/customer/${id}`)
+    return result.data
+}
+export const addCustomer = async (value) => {
+    const result = await axios.post(`http://localhost:2000/customer`,{ ...value })
+    return result.data
+}
+export const editCustomer = async (id,value) => {
+    const result = await axios.put(`http://localhost:2000/customer/${id}`,{ ...value })
+    return result.data
+}
+export const findById = async (id) => {
+    const result = await axios.get(`http://localhost:2000/customer/${id}`)
+    return result.data
+}
